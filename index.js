427c476be80aa6c7cef4da9932e5749d7d256e64
julianapedroso <juliana.cristina@hotmail.com.br>
2021-11-22 14:31:05 -0300

    :sparkles: body parser added

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const bodyParser = ('body-parser');
 
+// adicionando ejs:
 app.set('view engine', 'ejs');
+// adicionando arquivos estáticos:
 app.use(express.static('public'));
+// adicionando body parser:
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+// adicionando leitura via json:
+app.use(bodyParser.json());
 
+// Rotas
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -12,6 +22,11 @@ app.get('/ask', (req, res) => {
     res.render('ask');
 });
 
+app.post('/save-question', (req, res) => {
+    res.send("Formulário recebido");
+})
+
+// subindo servidor:
 app.listen(8080, () => {
     console.log('Server running on port 8080');
 });
